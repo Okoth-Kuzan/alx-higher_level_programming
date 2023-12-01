641bb9f3de07777aa6f78887c708f471dda583ce
Okoth-Kuzan <terrance.e.okoth47@icloud.com>
2023-11-30 17:11:36 -0800

    100

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -3,7 +3,6 @@ if __name__ == "__main__":
     a = 10
     b = 5
     from calculator_1 import add, sub, mul, div
-
     print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
     print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
     print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
