6e09fc852c70614554c19a80f6b384627538d808
Okoth-Kuzan <terrance.e.okoth47@icloud.com>
2023-12-06 14:54:15 -0800

    2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,9 +1,5 @@
 #!/usr/bin/python3
 def uniq_add(my_list=[]):
-    s = 0
-    hashet = dict()  # creating an empty hashet.
-    for i in range(len(my_list)):
-        if (my_list[i] not in hashet.keys()):  # if it's not in the hashet,
-            hashet[my_list[i]] = my_list[i]    # it'll be added.
-            s += my_list[i]
-    return(s)
+    unique_sum = sum(set(my_list))
+    return unique_sum
+
