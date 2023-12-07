b2db2e96328ec3a76bed6f2e03de0b28a3f8dd9a
Okoth-Kuzan <terrance.e.okoth47@icloud.com>
2023-12-07 11:52:58 -0800

    100

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -4,7 +4,7 @@ def roman_to_int(roman_string):
         return 0
     roman_dict =
     {
-            'I': 1,'V': 5,'X': 10,'L': 50,'C': 100,'D': 500,'M': 1000
+            'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
     }
     decs = [roman_dict[x] for x in roman_string]
     output = 0
