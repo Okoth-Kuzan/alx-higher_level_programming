9f9dde29bc87b838a23afd1dbdadecf39aad5cfc
Okoth-Kuzan <terrance.e.okoth47@icloud.com>
2023-12-07 11:26:45 -0800

    12

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,14 +1,6 @@
+#!/usr/bin/python3
 def best_score(a_dictionary):
     if not a_dictionary:
-        return None
+        return (None)
 
-    max_value = max(a_dictionary.values())
-    best_key = None
-
-    for key, value in a_dictionary.items():
-        if value == max_value:
-            best_key = key
-            break
-
-    return best_key
-
+    return (max(a_dictionary, key=a_dictionary.get))
---
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -0,0 +1,4 @@
+def multiply_list(my_list=[], number=0):
+    multiplied_list = [element * number for element in my_list]
+    return multiplied_list
+
